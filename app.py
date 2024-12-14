9a7c751d6be7c78e24689985b2ec9e1de793bd8a
ghaechka <sofie.litvin@gmail.com>
2024-12-14 12:39:33 +0500

    fixes

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template
+from flask_restx import Api
 
-from app.controllers.admin import admin_bp
-from app.controllers.tours.routes import tours_bp
-from app.controllers.tours import tours_bp1
+from app.controllers.tours.tours import tours_ns
+from app.controllers.admin.admin import admin_ns
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.secret_key = 'tour_agent'
-app.register_blueprint(admin_bp, url_prefix='/api')
-app.register_blueprint(tours_bp)
-app.register_blueprint(tours_bp1, url_prefix='/api')
+
+api = Api(
+    app,
+    title='Tour agent API',
+    prefix='/api',
+    default='API',
+    doc='/api/swagger',
+    default_label='General operations'
+)
+
+api.add_namespace(admin_ns, path='/api/admin')
+api.add_namespace(tours_ns, path='/api/tours')
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # todo: у меня не находил тут маршрут файла
+    return '??'
 
 
 if __name__ == "__main__":
--- app/controllers/tours/__init__.py
+++ app/controllers/tours/__init__.py
@@ -1,15 +1,1 @@
-from flask import Blueprint
-from flask_restx import Api
 
-from .tours import tours_ns
-
-tours_bp1 = Blueprint(
-    'routes_bp1', __name__
-)
-api = Api(
-    tours_bp1,
-    title='Tours API',
-    description='A RESTful API for Tours'
-)
-
-api.add_namespace(tours_ns, path='/tours')
