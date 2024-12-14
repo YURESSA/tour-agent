30457742805db201307f9e2f38fa21358cb702ac
YURESSA <goshrenko@gmail.com>
2024-12-14 12:45:50 +0500

    ddd

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template
-
+from flask_login import LoginManager, login_required
 from app.controllers.admin import admin_bp
 from app.controllers.tours.routes import tours_bp
 from app.controllers.tours import tours_bp1
+from app.controllers.admin.admin import User
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 app.secret_key = 'tour_agent'
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 app.register_blueprint(admin_bp, url_prefix='/api')
 app.register_blueprint(tours_bp)
 app.register_blueprint(tours_bp1, url_prefix='/api')
 
 
 @app.route('/')
+@login_required
 def home():
     return render_template('index.html')
+
+
+@app.route('/login')
+def get():
+    return render_template('login.html')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(1, 'admin', '@@@@', '8888')
 
 
 if __name__ == "__main__":
--- app/controllers/admin/admin.py
+++ app/controllers/admin/admin.py
@@ -1,11 +1,25 @@
-from flask import render_template
-from flask_restx import Namespace, Resource, fields
+import flask
+from flask import redirect, url_for, render_template
+from flask_login import login_user, UserMixin, logout_user
+from flask_restx import Namespace, Resource
+
 from app.controllers.admin.models.admin import admin
 
 admin_ns = Namespace(
     'admin',
     description='Admin operations'
 )
+
+
+class User(UserMixin):
+    def __init__(self, id, username, email, password):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.password = password
+
+    def get_id(self):
+        return str(self.id)
 
 
 @admin_ns.route('/')
@@ -25,3 +39,19 @@ class AdminGetOneController(Resource):
     def delete(self, admin_id):
         admin.delete(admin_id)
         return 204
+
+
+@admin_ns.route('/login')
+class UserLogin(Resource):
+    def post(self):
+        user = User(1, 'admin', '@@@@', '8888')
+        login_user(user)
+        flask.flash('Logged in successfully.')
+        return {'success': True}
+
+
+@admin_ns.route('/logout')
+class UserLogout(Resource):
+    def post(self):
+        logout_user()
+        return {'logout': True}
